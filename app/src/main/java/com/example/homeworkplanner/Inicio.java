e10c849e3e610df3911a501972c3afa6fdee7c7f
Dpizarro40 <d.pizarro40@alumnos.santotomas.cl>
2023-10-05 16:01:43 -0300

    Actualizacion 16:01 05/10/2023

--- app/src/main/java/com/example/homeworkplanner/Inicio.java
+++ app/src/main/java/com/example/homeworkplanner/Inicio.java
@@ -1,6 +1,9 @@
 package com.example.homeworkplanner;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,8 +11,14 @@ import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Inicio extends AppCompatActivity {
     FloatingActionButton aniadir;
+    RecyclerView listaTareas;
+    List<String> itemList;
+    ItemAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +32,33 @@ public class Inicio extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        listaTareas = findViewById(R.id.rvLista);
+        listaTareas.setLayoutManager(new LinearLayoutManager(this));
+
+        itemList = new ArrayList<>();
+        adapter = new ItemAdapter(itemList);
+        listaTareas.setAdapter(adapter);
+
+
+    }
+
+    public void addItem(View view) {
+        Intent intent = new Intent(this, CrearTarea.class);
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            if (data != null) {
+                String newItem = data.getStringExtra("newItem");
+                itemList.add(newItem);
+                adapter.notifyDataSetChanged();
+            }
+        }
     }
 
 }
--- app/src/main/java/com/example/homeworkplanner/MainActivity.java
+++ app/src/main/java/com/example/homeworkplanner/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
 
+
 public class MainActivity extends AppCompatActivity {
     Button botonInicio;
     @Override
